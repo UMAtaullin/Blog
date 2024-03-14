e82f2989669898211d2352a43b37c37d41133e4c
Ural Ataullin <ataullinural@gmail.com>
2024-03-14 16:15:09 +0500

    start templates

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'posts/index.html')
 
 
-def group_posts(request, slug):
-    return HttpResponse('Сообщества')
+def group_list(request, slug):
+    return render(request, 'posts/group_list.html')
