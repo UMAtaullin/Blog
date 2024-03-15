33fb1112b13d5b432529dde047be8404933d93a3
Ural Ataullin <ataullinural@gmail.com>
2024-03-15 16:07:29 +0500

    def index and group_list

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def index(request):
-    return render(request, 'posts/index.html')
+
+    posts = Post.objects.all()
+
+    data = {
+        'posts': posts,
+    }
+
+    return render(request, 'posts/index.html', data)
 
 
 def group_list(request, slug):
-    return render(request, 'posts/group_list.html')
+
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group)
+
+    data = {
+        'group': group,
+        'posts': posts,
+    }
+
+    return render(request, 'posts/group_list.html', data)
